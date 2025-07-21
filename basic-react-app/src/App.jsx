b38f822fec1ae5f0ff4c035ef95928ba989b9579
Manoj Dhage <manojdhage1341@gmail.com>
2025-07-21 21:22:58 +0530

    React first component created

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+
+
+
+function Description(){
+  return <h3>I am the Desciption</h3>;
+}
+
+function App() {
+
+  return (
+    <div>
+      <Title/>
+      <Description/>
+    </div>
+  
+  )
+}
+
+export default App
