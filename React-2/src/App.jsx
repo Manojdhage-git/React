5511fb926373d6590508a2f35044e1d49ad581d8
Manoj Dhage <manojdhage1341@gmail.com>
2025-07-29 20:30:00 +0530

    LikeButon Implemented

--- React-2/src/App.jsx
+++ React-2/src/App.jsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './Counter'
+import LikeButton from './LikeButton.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return (<div>
+     <LikeButton/>
     <Counter/>
+
+  </div>
+   
   )
 }
 
