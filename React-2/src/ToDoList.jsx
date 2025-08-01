7dc6247a5c6b51deb617d3c61b7065f8a5b21ea7
Manoj Dhage <manojdhage1341@gmail.com>
2025-08-01 18:21:00 +0530

    create to do

--- React-2/src/ToDoList.jsx
+++ React-2/src/ToDoList.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
+
 export default function ToDoList(){
+    let [todos,setTodos] = useState(["sample task 1", "sample task 2"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTodo = () => {
+        setTodos([...todos, newTodo]);
+        setNewTodo(""); // Clear the input field after adding the task
+    }
+
+    let updateTadoValue=(event)=>{
+
+        setNewTodo(event.target.value);
+    }
+
     return (
         <div>
-            <input placeholder="Add a task"/> 
+            <input placeholder="Add a task" value={newTodo} onChange={updateTadoValue}/> 
             <br />
-            <button>Add Task</button>   
+            <button onClick={addNewTodo}>Add Task</button>   
            
             <br />
             <br />
@@ -11,6 +26,11 @@ export default function ToDoList(){
             <hr />
 
             <h4>Tasks To Do</h4>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
         </div>
     )
 }
